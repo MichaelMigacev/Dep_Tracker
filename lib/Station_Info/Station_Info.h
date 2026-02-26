b8d2124c66c053a805f50e55ff6aa476a31ab199
Michael Migacev <micha.migacev@googlemail.com>
2026-02-26 16:09:04 +0100

    Added Station Information Extraction from the bvg API

---
+++ lib/Station_Info/Station_Info.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <Arduino.h>
+#include <ArduinoJson.h>
+
+static String QUERY_SIZE = "8";              // How many journeys to query (should be >= LCD lines)
+static constexpr uint8_t LCD_SIZE_ROWS = 4U; // 4 rows standard
+struct DepartureList
+{
+    String departures[LCD_SIZE_ROWS]; // Fixed size for LCD lines
+    int count;                        // How many departures are actually filled
+};
+
+class StationInfo
+{
+public:
+    DepartureList getSouthboundJourneys(String fromStation, String toStation);
+
+private:
+    DepartureList parseJourneys(String jsonResponse);
+    String makeJourneyMessage(String lineName, String direction, int diffMinutes);
+    String cutString(String input, uint8_t maxLength);
+    String cleanStationName(String stationName);
+    String removeParentheses(String input);
+    String formatTimeDisplay(int minutes);
+};
